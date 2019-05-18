74237999c28bd20bf726aefce8a4cc6710372253
silvonei <silvoneildr@gmail.com>
2019-05-18 16:25:38 -0300

    db connection and post users

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,8 +5,19 @@ const User = require('../models/user.js');
 const router = express.Router();
 
 router.post('/register', async (req, res) => {
+  const { email, name, id } = req.body;
   try {
+    if (await User.findOne({ id }))
+      return res.status(400).send({ error: 'Id already exists!' });
+
+    if (await User.findOne({ name }))
+      return res.status(400).send({ error: 'Name already exists!' });
+
+    if (await User.findOne({ email }))
+      return res.status(400).send({ error: 'E-mail already exists!' });
+
     const user = await User.create(req.body);
+    user.password = undefined;
     return res.send({ user });
   } catch (error) {
     return res.status(400).send({ error: 'Registration failed!' });
--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/aluno10db', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost/aluno10db', { useNewUrlParser: true, useCreateIndex: true, });
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
+
+
