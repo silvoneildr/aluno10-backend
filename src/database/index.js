6d95cf2db91eea49feeede2a13c444aba60c2685
silvonei <silvoneildr@gmail.com>
2019-05-31 15:39:57 -0300

    Add configs mongo db on mlab

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,14 @@
 const mongoose = require('mongoose');
 
 mongoose.set('useFindAndModify', false);
-mongoose.connect('mongodb://localhost/aluno10db', { useNewUrlParser: true, useCreateIndex: true, });
+// mongodb://<dbuser>:<dbpassword>@ds041821.mlab.com:41821/aluno10db
+// mongodb://localhost/aluno10db
+
+mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds041821.mlab.com:41821/aluno10db', {
+  useNewUrlParser: true,
+  useCreateIndex: true
+});
+
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
