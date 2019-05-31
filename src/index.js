cc30e4962f2c940157a7c6a2a0ead783c60055e8
silvonei <silvoneildr@gmail.com>
2019-05-31 16:47:40 -0300

    Updates index server

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const Student = require('./app/models/student');
 
-const PORT = 3000;
-const HOST = '0.0.0.0';
+// const PORT = 3000;
+// const HOST = '0.0.0.0';
 
 const app = express();
 
@@ -11,15 +10,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-  // res.send('Backend is OK!');
-  try {
-    const students = await Student.find({});
-    return res.send({ students });
-  } catch (error) {
-    return res.status(400).send({ error: 'Request failed!' });
-  }
+  res.send('Backend is OK!');
 });
 
 require('./app/controllers/index')(app);
 
-app.listen(process.env.PORT, HOST);
+app.listen(process.env.PORT);
