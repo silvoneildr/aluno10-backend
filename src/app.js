059fefbee2971d65dd4a972a926f0ac98cd48b05
Everton Arakaki <eveuca@gmail.com>
2019-06-19 16:58:38 -0300

    adding configuration file

--- src/app.js
+++ src/app.js
@@ -1,27 +1,26 @@
+// load config variables
+const config = require('./config/config.js');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const allowCors = require('./config/cors')
 const path = require('path');
 
-const PORT = 3000;
-const HOST = '0.0.0.0';
+const PORT = global.gConfig.node_port;
+const HOST = global.gConfig.node_host;
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(allowCors);
-app.use('/', express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-    res.status(200).send({
-        title: "Node Express API",
-        version: "0.0.1"
-    });
+    res.json(global.gConfig);
 });
 
 require('./app/controllers/index')(app);
 
-app.listen(PORT,HOST);
+app.listen(PORT, HOST);
 
 console.log('Backend is OK');
